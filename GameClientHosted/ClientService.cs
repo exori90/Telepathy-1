d6794334c0959fa9a88810352674b601de153ace
x-gustavs <gustav.sivander@hotmail.com>
2024-06-25 15:57:15 +0200

    Update ClientService.cs

--- GameClientHosted/ClientService.cs
+++ GameClientHosted/ClientService.cs
@@ -21,69 +21,15 @@ public class ClientService : IHostedService
     {
         Thread.Sleep(5000);
         StartClients("127.0.0.1", 1337, 10, 0);
+
+        DoWork(null);
         
         return Task.CompletedTask;
     }
 
     private void DoWork(object? state)
     {
-        foreach (Client client in clients)
-        {
-            if (client.Connected)
-            {
-                int sendCount = 1;
-                // send 2 messages each time
-                for (int i = 0; i < sendCount; i++)
-                {
-                    string message = $"Message {i}";
-                    byte[] messageBytes = Encoding.ASCII.GetBytes(message);
-                    client.Send(new ArraySegment<byte>(messageBytes));
-                }
-
-                messagesSent += sendCount;
-
-                // tick client to receive and update statistics in OnData
-                client.Tick(1);
-            }
-        }
-    }
-
-    public Task StopAsync(CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-
-    public void StartClients(string host, int port, int clientAmount, int seconds)
-    {
-        Log.Error("[Telepathy] Starting " + clientAmount + " clients...");
-
-        // start n clients and get queue messages all in this thread
-
-        
-        for (int i = 0; i < clientAmount; ++i)
-        {
-            Client client = new Client(MaxMessageSize);
-            // setup hook to add to statistics
-            client.OnData = data => {
-                Log.Info($"Server sends: {Encoding.ASCII.GetString(data.ToArray(), 0, data.Count)}");
-                messagesReceived++;
-                dataReceived += data.Count;
-            };
-            client.Connect(host, port);
-            clients.Add(client);
-            Thread.Sleep(15);
-        }
-        Log.Info("[Telepathy] Started all clients");
-
-        // make sure that all clients connected successfully. otherwise
-        // the sleep might be too small, or other reasons. no point in
-        // load testing if the connect failed already.
-        if (!clients.All(cl => cl.Connected))
-        {
-            Log.Info("[Telepathy] Not all clients were connected successfully. aborting.");
-            return;
-        }
-
+        int seconds = 0;
         Stopwatch stopwatch = Stopwatch.StartNew();
 
 
@@ -133,6 +79,37 @@ public class ClientService : IHostedService
         foreach (Client client in clients)
         {
             client.Disconnect();
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public void StartClients(string host, int port, int clientAmount, int seconds)
+    {
+        Log.Error("[Telepathy] Starting " + clientAmount + " clients...");
+ 
+        for (int i = 0; i < clientAmount; ++i)
+        {
+            Client client = new Client(MaxMessageSize);
+            // setup hook to add to statistics
+            client.OnData = data => {
+                Log.Info($"Server sends: {Encoding.ASCII.GetString(data.ToArray(), 0, data.Count)}");
+                messagesReceived++;
+                dataReceived += data.Count;
+            };
+            client.Connect(host, port);
+            clients.Add(client);
+            Thread.Sleep(15);
+        }
+        Log.Info("[Telepathy] Started all clients");
+
+        if (!clients.All(cl => cl.Connected))
+        {
+            Log.Info("[Telepathy] Not all clients were connected successfully. aborting.");
+            return;
         }
     }
 }
